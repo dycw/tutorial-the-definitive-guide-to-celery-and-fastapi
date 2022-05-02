8cb53ba15c9a96a9b4a7d6133e072694bf5c34e0
Derek Wan <d.wan@icloud.com>
2022-05-02 15:04:10 +0800

    run worker is up there too

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+from subprocess import check_call  # noqa: S404
+
+from beartype import beartype
+from watchgod import run_process
+
 from project import create_app
 
 
@@ -5,20 +10,17 @@ app = create_app()
 celery = app.celery_app  # type: ignore
 
 
+@beartype
+def celery_worker() -> None:
+    _ = run_process("./project", _run_worker)
+
+
+@beartype
+def _run_worker() -> None:
+    _ = check_call(  # noqa: S603, S607
+        ["celery", "-A", "main.celery", "worker", "--loglevel=info"]
+    )
+
+
 if __name__ == "__main__":
-    from subprocess import check_call  # noqa: S404
-
-    from beartype import beartype
-    from watchgod import run_process
-
-    @beartype
-    def celery_worker() -> None:
-        _ = run_process("./project", _run_worker)
-
-    @beartype
-    def _run_worker() -> None:
-        _ = check_call(  # noqa: S603, S607
-            ["celery", "-A", "main.celery", "worker", "--loglevel=info"]
-        )
-
     celery_worker()
