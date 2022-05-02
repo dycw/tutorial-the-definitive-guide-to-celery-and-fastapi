311f1d2dd99f62b5a5e67542ae085991ab4c9bb8
Derek Wan <d.wan@icloud.com>
2022-05-02 15:01:09 +0800

    Finish "Setting Up Auto-Reload" (#3)

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
-from project import create_app  # noqa: INP001
+from beartype import beartype
+from watchgod import run_process
+
+from project import create_app
 
 
 app = create_app()
 celery = app.celery_app  # type: ignore
+
+
+if __name__ == "__main__":
+
+    from subprocess import check_call  # noqa: S404
+
+    @beartype
+    def celery_worker() -> None:
+        _ = run_process("./project", _run_worker)
+
+    @beartype
+    def _run_worker() -> None:
+        _ = check_call(  # noqa: S603, S607
+            ["celery", "-A", "main.celery", "worker", "--loglevel=info"]
+        )
+
+    celery_worker()
