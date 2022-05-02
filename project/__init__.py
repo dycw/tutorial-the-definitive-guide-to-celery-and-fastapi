b86a58dbde40e47c45431e2fd5eedce0c359d258
Derek Wan <d.wan@icloud.com>
2022-05-02 10:25:29 +0800

    Update pyproject.toml

--- project/__init__.py
+++ project/__init__.py
@@ -4,6 +4,10 @@ from fastapi import FastAPI
 def create_app() -> FastAPI:
     app = FastAPI()
 
+    from project.users import users_router
+
+    app.include_router(users_router)
+
     @app.get("/")
     async def root() -> dict[str, str]:  # type: ignore
         return {"message": "Hello World"}
---
+++ project/users/models.py
@@ -0,0 +1,22 @@
+from typing import Any
+
+from sqlalchemy import Column
+from sqlalchemy import Integer
+from sqlalchemy import String
+
+from project.database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    username = Column(String(128), unique=True, nullable=False)
+    email = Column(String(128), unique=True, nullable=False)
+
+    def __init__(
+        self, username: str, email: str, *args: Any, **kwargs: Any
+    ) -> None:
+        super().__init__()
+        self.username = username
+        self.email = email
