b6426f9a869eabe4a86fde044c1a17879ce161f0
Derek Wan <d.wan@icloud.com>
2022-05-02 15:24:22 +0800

    Add RDB

--- project/users/tasks.py
+++ project/users/tasks.py
@@ -5,6 +5,9 @@ from celery import shared_task
 @shared_task(name="project.users.tasks.divide")
 @beartype
 def divide(x: float, y: float) -> float:
+    # from celery.contrib.rdb import set_trace # noqa: E800
+    # set_trace() # noqa: E800
+
     from time import sleep
 
     sleep(3)
