a07a54d2f5a866f7020477f41f340478b8cea2f1
Derek Wan <d.wan@icloud.com>
2022-05-02 17:39:33 +0800

    Continue despite

--- project/users/tasks.py
+++ project/users/tasks.py
@@ -12,3 +12,11 @@ def divide(x: float, y: float) -> float:
 
     sleep(3)
     return x / y
+
+
+@shared_task(name="project.users.tasks.sample_task")
+@beartype
+def sample_task(email: str) -> None:
+    from project.users.views import api_call
+
+    api_call(email)
