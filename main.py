e3210667b0b68ea3c7fd10ed05d1e2410e834b2f
Derek Wan <d.wan@icloud.com>
2022-05-02 15:00:20 +0800

    this

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-from subprocess import check_call  # noqa: S404
-
 from beartype import beartype
 from watchgod import run_process
 
@@ -10,17 +8,18 @@ app = create_app()
 celery = app.celery_app  # type: ignore
 
 
-@beartype
-def celery_worker() -> None:
-    _ = run_process("./project", _run_worker)
-
-
-@beartype
-def _run_worker() -> None:
-    _ = check_call(  # noqa: S603, S607
-        ["celery", "-A", "main.celery", "worker", "--loglevel=info"]
-    )
-
-
 if __name__ == "__main__":
+
+    from subprocess import check_call  # noqa: S404
+
+    @beartype
+    def celery_worker() -> None:
+        _ = run_process("./project", _run_worker)
+
+    @beartype
+    def _run_worker() -> None:
+        _ = check_call(  # noqa: S603, S607
+            ["celery", "-A", "main.celery", "worker", "--loglevel=info"]
+        )
+
     celery_worker()
